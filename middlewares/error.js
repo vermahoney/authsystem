8619e4a4ddcee21825714ac7ae42dd4b5956e05d
vermahoney <vishusunar7@gmail.com>
2026-07-20 22:41:06 +0530

    error handling

---
+++ middlewares/error.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+const httpStatus = require('http-status');
+const config = require('../cpnfig/config');
+const logger = require('../config/logger');
+const ApiError = require('../utils/ApiError');
