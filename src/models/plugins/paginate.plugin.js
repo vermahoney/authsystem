8f9227691847ac90ab4d9b8268c43427b50f0cf3
vermahoney <vishusunar7@gmail.com>
2026-08-04 17:46:02 +0530

    add paginate

--- src/models/plugins/paginate.plugin.js
+++ src/models/plugins/paginate.plugin.js
@@ -1,5 +1,26 @@
 const paginate = (schema) => {
+  schema.statics.paginate = async function (
+    filter,
+    options
+  ) {
+     let sort = "";
 
+if (options.sortBy) {
+  const sortingCriteria = [];
+
+  options.sortBy.split(",").forEach((sortOption) => {
+    const [key, order] = sortOption.split(":");
+
+    sortingCriteria.push(
+      (order === "desc" ? "-" : "") + key
+    );
+  });
+
+  sort = sortingCriteria.join(" ");
+} else {
+  sort = "createdAt";
+}
+  };
 };
 
 export default paginate;
