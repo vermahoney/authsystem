e2bf14a7d072d9266d2e0ef54eadad81e34ea443
vermahoney <vishusunar7@gmail.com>
2026-08-06 12:46:37 +0530

    updated  paginate

--- src/models/plugins/paginate.plugin.js
+++ src/models/plugins/paginate.plugin.js
@@ -3,23 +3,44 @@ const paginate = (schema) => {
     filter,
     options
   ) {
-     let sort = "";
+    let sort = "";
 
-if (options.sortBy) {
-  const sortingCriteria = [];
+    if (options.sortBy) {
+      const sortingCriteria = [];
 
-  options.sortBy.split(",").forEach((sortOption) => {
-    const [key, order] = sortOption.split(":");
+      options.sortBy.split(",").forEach((sortOption) => {
+        const [key, order] = sortOption.split(":");
 
-    sortingCriteria.push(
-      (order === "desc" ? "-" : "") + key
-    );
-  });
+        sortingCriteria.push(
+          (order === "desc" ? "-" : "") + key
+        );
+      });
 
-  sort = sortingCriteria.join(" ");
-} else {
-  sort = "createdAt";
-}
+      sort = sortingCriteria.join(" ");
+    } else {
+      sort = "createdAt";
+    }
+
+    const limit =
+      options.limit && parseInt(options.limit, 10) > 0
+        ? parseInt(options.limit, 10)
+        : 10;
+
+    const page =
+      options.page && parseInt(options.page, 10) > 0
+        ? parseInt(options.page, 10)
+        : 1;
+
+    const skip = (page - 1) * limit;
+
+    // 👇 YAHAN ADD KARNA HAI
+    const countPromise = this.countDocuments(filter).exec();
+
+    const docsPromise = this.find(filter)
+      .sort(sort)
+      .skip(skip)
+      .limit(limit)
+      .exec();
   };
 };
 
