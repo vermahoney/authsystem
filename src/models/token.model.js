ab082581834f559976ea40260f47f6a7e704080c
vermahoney <vishusunar7@gmail.com>
2026-08-10 19:26:43 +0530

    add token.model file

--- src/models/token.model.js
+++ src/models/token.model.js
@@ -1,9 +1,41 @@
 import mongoose from "mongoose";
-import { tokenTypes } from "../config/tokens.js";
+import tokenTypes from "../config/tokens.js";
 
 const tokenSchema = new mongoose.Schema(
-  {},
+  {
+    token: {
+      type: String,
+      required: true,
+      index: true,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    type: {
+      type: String,
+      enum: Object.values(tokenTypes),
+      required: true,
+    },
+
+    expires: {
+      type: Date,
+      required: true,
+    },
+
+    blacklisted: {
+      type: Boolean,
+      default: false,
+    },
+  },
   {
     timestamps: true,
   }
 );
+
+const Token = mongoose.model("Token", tokenSchema);
+
+export default Token;
