8ba73cd948bb0989364c5a624672b405872d80ea
vermahoney <vishusunar7@gmail.com>
2026-08-12 16:54:31 +0530

    adding auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,1 +1,42 @@
+import httpStatus from "http-status";
+import ApiError from "../utils/ApiError.js";
+import tokenService from "../services/token.service.js";
+import User from "../models/user.model.js";
+import tokenTypes from "../config/tokens.js";
 
+const auth = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      throw new ApiError(
+        httpStatus.UNAUTHORIZED,
+        "Please authenticate"
+      );
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const payload = await tokenService.verifyToken(
+      token,
+      tokenTypes.ACCESS
+    );
+
+    const user = await User.findById(payload.sub);
+
+    if (!user) {
+      throw new ApiError(
+        httpStatus.UNAUTHORIZED,
+        "User not found"
+      );
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default auth;
