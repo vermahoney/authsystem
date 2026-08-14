a403e80a2c541cb1708dd0f4c229ff957959b5e8
vermahoney <vishusunar7@gmail.com>
2026-08-14 12:41:11 +0530

    initialize the passport.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import userRoute from "./src/routes/user.routes.js";
+import passport from "passport";
+import jwtStrategy from "./src/config/passport.js";
 import {
   errorConverter,
   errorHandler,
@@ -8,6 +10,11 @@ import routes from "./src/routes/index.js";
 
 const app = express();
 app.use(express.json());
+passport.use("jwt", jwtStrategy);
+
+app.use(passport.initialize());
+app.use(errorConverter);
+app.use(errorHandler);
 
 app.use("/v1", routes);
 
