e4699cfef0c37e6b12518a1fb6e39ad9c62a6e7b
vermahoney <vishusunar7@gmail.com>
2026-08-19 17:27:19 +0530

    fixing the app.js  by removing the duplicate

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 import express from "express";
-import userRoute from "./src/routes/user.routes.js";
+
 import passport from "passport";
 import jwtStrategy from "./src/config/passport.js";
+
+import routes from "./src/routes/index.js";
+
 import {
   errorConverter,
   errorHandler,
 } from "./src/middlewares/error.js";
-import routes from "./src/routes/index.js";
 
 const app = express();
+
 app.use(express.json());
+
+// Passport
 passport.use("jwt", jwtStrategy);
-
 app.use(passport.initialize());
-app.use(errorConverter);
-app.use(errorHandler);
 
+// Routes
 app.use("/v1", routes);
-
-app.use("/user", userRoute);
 
 // Error Middlewares
 app.use(errorConverter);
 app.use(errorHandler);
 
 export default app;
-
